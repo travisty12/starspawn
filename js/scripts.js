6c41c3aa07e63ffda412f2d47621c7399822c9d2
Ryan McLean and Travis Toal <ryan@>
2019-02-04 11:13:17 -0800

    add grid/move function

--- js/scripts.js
+++ js/scripts.js
@@ -8,7 +8,6 @@ function PacMan() {
   this.nextY=0;
   this.dots = 0;
   this.dir = 0;
-  this.speed = 1;
 }
 
 
@@ -22,40 +21,70 @@ PacMan.prototype.move = function(event) {
   } else if (event.which == 40) {
     this.dir = 4;
   }
-  slide = setInterval(moveMore(), 500);
+  switch (pacMan.dir) {
+    case 0:
+      break;
+    case 1:
+      pacMan.nextY -= 1;
+      updatePos(pacMan);
+      pacMan.y = pacMan.nextY;
+      break;
+    case 2:
+      pacMan.nextX -= 1;
+      updatePos(pacMan);
+      pacMan.x = pacMan.nextX;
+      break;
+    case 3:
+      pacMan.nextY += 1;
+      updatePos(pacMan);
+      pacMan.y = pacMan.nextY;
+      break;
+    case 4:
+      pacMan.nextX += 1;
+      updatePos(pacMan);
+      pacMan.x = pacMan.nextX;
+      break;
+  }
+  if (pacMan.nextX < 0) {
+    pacMan.nextX = 0;
+  }
+  if (pacMan.nextY < 0) {
+    pacMan.nextY = 0;
+  }
+  if (pacMan.nextX == Math.sqrt($(".grid").length)) {
+    pacMan.nextX = Math.sqrt($(".grid").length) - 1;
+  }
+  if (pacMan.nextY == Math.sqrt($(".grid").length)) {
+    pacMan.nextY = Math.sqrt($(".grid").length) - 1;
+  }
   updatePos(this);
   this.x = this.nextX;
   this.y = this.nextY;
 }
 
 function updatePos(pac) {
-  $("#col" + pac.x + "-" + pac.y).addClass("emptyCell");
-  $("#col" + pac.x + "-" + pac.y).removeClass("pacAt");
-  $("#col" + pac.nextX + "-" + pac.nextY).removeClass("emptyCell");
-  $("#col" + pac.nextX + "-" + pac.nextY).addClass("pacAt");
-  var hasDot=$("#col" + pac.nextX + "-" + pac.nextY).hasClass("dot");
+  $("#cell" + pac.x + "-" + pac.y).addClass("emptyCell");
+  $("#cell" + pac.x + "-" + pac.y).removeClass("pacAt");
+  $("#cell" + pac.nextX + "-" + pac.nextY).removeClass("emptyCell");
+  $("#cell" + pac.nextX + "-" + pac.nextY).addClass("pacAt");
+  var hasDot=$("#cell" + pac.nextX + "-" + pac.nextY).hasClass("dot");
   if(hasDot){
-    $("#col" + pac.nextX + "-" + pac.nextY).removeClass("dot");
+    $("#cell" + pac.nextX + "-" + pac.nextY).removeClass("dot");
     pac.dots++;
   }
   $("span#dotAmount").text(pac.dots);
 }
 
 var textBlock = "";
+
 function addGrid(size){
-  for (var i=0;i<=parseInt(size);i++){
-    grid[i] = [];
-    textBlock += "<div class='row'>";
-
-    for (var j=0;j<=parseInt(size);j++){
-      grid[i][j]=0;
-      textBlock += "<div class='dot emptyCell cell col-sm-" + (Math.floor(12/size)) + "' id='col" + i + "-" + j + "'></div>";
-
-    }
-    textBlock += "</div>";
-
-  }
-  $("#gridSpot").append(textBlock);
+  for (var rows = 0; rows < size; rows++) {
+    for (var columns = 0; columns < size; columns++) {
+      $("#gridSpot").append("<div class='grid dot' id='cell" + rows + "-" + columns + "'></div>");
+    };
+  };
+  $(".grid").width(960/size);
+  $(".grid").height(960/size);
 }
 var result = 0;
 function score(ban,che,pea,pin,str) {
@@ -87,52 +116,8 @@ var pineappleNum=0;
 var strawberryNum=0;
 
 
-var slide = setInterval(moveMore, 125);
-
-function moveMore() {
-  switch (pacMan.dir) {
-    case 0:
-      break;
-    case 1:
-      pacMan.nextY -= 1;
-      updatePos(pacMan);
-      pacMan.y = pacMan.nextY;
-      break;
-    case 2:
-      pacMan.nextX -= 1;
-      updatePos(pacMan);
-      pacMan.x = pacMan.nextX;
-      break;
-    case 3:
-      pacMan.nextY += 1;
-      updatePos(pacMan);
-      pacMan.y = pacMan.nextY;
-      break;
-    case 4:
-      pacMan.nextX += 1;
-      updatePos(pacMan);
-      pacMan.x = pacMan.nextX;
-      break;
-  }
-  if (pacMan.nextX < 0) {
-    pacMan.nextX = grid.length - 1;
-  }
-  if (pacMan.nextY < 0) {
-    pacMan.nextY = grid.length - 1;
-  }
-  if (pacMan.nextX == grid.length) {
-    pacMan.nextX = 0;
-  }
-  if (pacMan.nextY == grid.length) {
-    pacMan.nextY = 0;
-  }
-  updatePos(pacMan);
-  pacMan.x = pacMan.nextX;
-  pacMan.y = pacMan.nextY;
-}
-
 $(document).ready(function() {
-  addGrid(11);
+  addGrid(10);
   updatePos(pacMan);
   document.addEventListener("keydown", function(event) {
     pacMan.move(event);

8759b874620a1d1685354de487ba4186aa80ccc4
Jared Farkas and Ryan McLean <ryan@>
2019-02-05 10:30:39 -0800

    add gameover & restart functions

--- js/frame.js
+++ js/frame.js
@@ -1,10 +1,12 @@
 function Fire(){
-  this.life = 100;
+  this.life = 5;
 }
 
+var fire = new Fire();
+var player = new Player();
 function Player(){
   this.pos = [50,50];
-  this.inventory = [{type: wood, amount: 0}];
+  this.inventory = [{type: "wood", amount: 0}];
   this.score = 0;
   this.time = 0;
 }
@@ -15,24 +17,38 @@ function Player(){
 
 
 Player.prototype.cutTree = function(arr){
-    //arr;
-    this.inventory[0].amount += 1;
+    arr.forEach(function(ar){
+      this.inventory[0].amount++;
+      trees--;
+    })
 };
 
-Player.prototype.addToFire = function(theFire){
-  if(player.inventory[0].amount > 0){
+Player.prototype.addToFire = function(){
+  if(this.inventory[0].amount > 0){
     this.inventory[0].amount -= 1;
-    theFire.life += 30;
-
+    fire.life += 30;
   }
 };
 
-
+var over = false;
 function gameOver(){
 $("#gridSpot").hide();
 $("#game-over").show();
+$(".bonfire").hide();
+$(".man").empty();
+$("#inventory").hide();
+over = true;
 }
 
 function gameRestart(){
-
+  $("#gridSpot").show();
+  $("#game-over").hide();
+  $(".bonfire").show();
+  $(".man").empty();
+  $("#inventory").show();
+  fire = new Fire();
+  myTimer();
+  myVar = setInterval(myTimer, 1000);
+  player = new Player();
+  over = false;
 }

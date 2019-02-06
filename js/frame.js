28ee3f069869035ad3529003892b862e377cc0a0
Student <example@example.com>
2019-02-06 11:38:29 -0800

    Refactor

--- js/frame.js
+++ js/frame.js
@@ -1,10 +1,10 @@
-function Fire(){
+function Fire() {
   this.life = 30;
 }
 
 var fire = new Fire();
 var player = new Player();
-function Player(){
+function Player() {
   this.pos = [50,50];
   this.inventory = [{type: "wood", amount: 0}];
   this.score = 0;
@@ -16,45 +16,40 @@ function Player(){
 // }
 
 
-Player.prototype.cutTree = function(arr){
+Player.prototype.cutTree = function(arr) {
   this.inventory[0].amount++;
-    arr.forEach(function(ar){
+    arr.forEach(function(ar) {
       map[map.indexOf(ar)] = [];
-    })
-    map = map.filter(function(ma){
-      if(ma.length > 1)
-      {
+    });
+    map = map.filter(function(ma) {
+      if(ma.length > 1) {
         return ma;
       }
-    })
-    console.log(map.length);
+    });
     $(".tree").remove();
     mapFill();
     setTimeout(refill, 10000);
     $("#wood").text(this.inventory[0].amount);
-
   }
 
-Player.prototype.addToFire = function(){
+Player.prototype.addToFire = function() {
     this.inventory[0].amount -= 10;
     this.score += 100;
     fire.life += 10;
     $("#wood").text(this.inventory[0].amount);
 };
 
-
 var over = false;
-function gameOver(){
-$("#gridSpot").hide();
-$("#game-over").show();
-$(".bonfire").hide();
-$("#inventory").hide();
-over = true;
-map = [];
-console.log(map);
+function gameOver() {
+  $("#gridSpot").hide();
+  $("#game-over").show();
+  $(".bonfire").hide();
+  $("#inventory").hide();
+  over = true;
+  map = [];
 }
 
-function gameRestart(){
+function gameRestart() {
   clearInterval(myVar);
   clearInterval(skellyMove);
   $("#gridSpot").show();
